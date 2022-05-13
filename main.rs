5274a3c782c18dc3e15883749a25c2eda264e7c2
João A. Ferreir <joao.parana@gmail.com>
2022-05-13 09:07:38 -0300

    ajustes

--- main.rs
+++ main.rs
@@ -1,7 +1,18 @@
+use std::fs::File;
 
 fn main() {
     let var: u8 = 128;
     println!("Olá mundo ! var = {}", var); 
     println!("var = {}, tamanho = {} bytes", var, std::mem::size_of_val(&var));
+
+    let f = File::open("main.rs");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Houve um problema ao abrir o arquivo: {:?}", error)
+        },
+    };
+
 }
 
